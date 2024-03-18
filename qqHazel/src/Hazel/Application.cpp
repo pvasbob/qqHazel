44fca0817d3dedb3adb88e17fc083783174a55b5
pvasbob <pvasbob@gmail.com>
2024-03-17 20:36:52 -0400

    Setup basic Application and Entry Point.

---
+++ qqHazel/src/Hazel/Application.cpp
@@ -0,0 +1,26 @@
+#include "Application.h"
+
+namespace Hazel {
+
+	Application::Application()
+	{
+
+	}
+
+
+	Application::~Application()
+	{
+
+	}
+
+	void Application::Run()
+	{
+		while (true);
+	}
+
+
+}
+
+
+
+
